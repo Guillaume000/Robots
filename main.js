0278e73027053caec9fbe45554d0f4e4f74450e6
Guillaume000 <38075069+Guillaume000@users.noreply.github.com>
2018-04-30 16:29:36 +0200

    1 - Créer une Classe Robot

---
+++ main.js
@@ -0,0 +1,9 @@
+var robot = new Robot("RoboTest");
+
+console.log(robot);
+console.log(robot.name);
+console.log(robot.lifePoints);
+console.log(robot.mobility);
+console.log(robot.positionX);
+console.log(robot.positionY);
+
---
+++ robot.js
@@ -0,0 +1,10 @@
+class Robot {
+    constructor(name, lifePoints, mobility, positionX, positionY) {
+        this.name = name;
+        this.lifePoints = 100;
+        this.mobility = 3;
+        this.positionX = 0;
+        this.positionY = 0;
+    }
+}
+
