d9dddcbbed0d409cf63399e9aaf53642247c88d0
Guillaume <g.erckelbout@gmail.com>
2018-04-30 17:43:46 +0200

    3 - Des Robots qui parlent

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var funnyBot = new Robot("Funnybot");
 var robotChien = new Robot("Robotchien");
 
-console.log(funnyBot);
-console.log(robotChien);
+//console.log(funnyBot);
+//console.log(robotChien);
 
+funnyBot.introduce();
+robotChien.introduce();
--- robot.js
+++ robot.js
@@ -1,10 +1,13 @@
 class Robot {
-    constructor(name, lifePoints, mobility, positionX, positionY) {
+    constructor(name, lifePoints, mobility, position) {
         this.name = name;
         this.lifePoints = 100;
         this.mobility = 3;
-        this.positionX = 0;
-        this.positionY = 0;
+        this.position = [0, 0];
+
+        this.introduce = function() {
+            console.log("Bonjour, je m'appelle " + this.name + ". J'ai " + this.lifePoints + " points de vie et je me déplace à " + this.mobility + " cases par seconde. " + "Je suis à la case de coordonnées (" + this.position + ").");
+        }
     }
 }
 
