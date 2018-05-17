99f64f18331870e77a5a075995cdbcdebb776fb2
Guillaume <g.erckelbout@gmail.com>
2018-05-17 11:44:10 +0200

    Ajout du bouton 'Fin du tour"  qui permet de passer du joueur 1 au joueur 2

--- etape 2/js/main.js
+++ etape 2/js/main.js
@@ -4,53 +4,79 @@ board.populateGridWith("wall");
 board.populateGridWith("weapon");
 board.populateGridWith("robot");
 
-var joueur1 = board.robots[0];
-var joueur2 = board.robots[1];
-//turn est = à true pour le joueur1 et à false pour le joueur2
-var turn = true;
+var player1 = board.robots[0];
+var player2 = board.robots[1];
+var wallsPosition = board.walls;
+//turn est = à true pour le player1 et à false pour le player2
+var turn = false;
 var victoryCondition = false;
 
-//Tours de jeu
-//while(turn) {
-    $(document).ready(function(){
-        console.log("Tour du Joueur 1");
-        $("#case-" + joueur1.position.a + joueur1.position.b).click(function(){
-            //Quand on clique, les cases accessibles sont en couleur
-            /*$("#case-" + joueur1.position.a + joueur1.position.b).hover(function(){
-                $("#case-" + (joueur1.position.a + 1) + (joueur1.position.b)).addClass('mobility');
-            });*/
-            $("#case-" + joueur1.position.a + joueur1.position.b).removeClass('robot0');
-            joueur1.move();
-            $("#case-" + joueur1.position.a + joueur1.position.b).addClass('robot0');
-            //Si le personnage arrive sur une case avec une arme, il prend l'arme et laisse la sienne sur place
-            //Si le personnage veut se déplacer à travers un mur ou un autre personnage, il s'arrête devant
-            //Si le personnage arrive à côté de l'autre personnage le combat commence
-        });
-    });
-    //Fin du tour du joueur1
-    //turn = false;
-//}
+//Fin du tour du joueur en cours
+$("#endTurn").click(function(){
+    if(turn == true) {
+        turn = false;
+    } else {
+        turn = true;
+    }
 
-//while(turn == false) {  
-    $(document).ready(function(){
-        console.log("Tour du Joueur 2");
-        $("#case-" + joueur2.position.a + joueur2.position.b).click(function(){
-            $("#case-" + joueur2.position.a + joueur2.position.b).removeClass('robot1');
-            joueur2.move();
-            $("#case-" + joueur2.position.a + joueur2.position.b).addClass('robot1');
+    //Tours de jeu
+    if(turn == true) {
+        $(document).ready(function(){
+            console.log("Tour du Joueur 1");
+            $("#case-" + player1.position.a + player1.position.b).hover(function() {
+                for(var x = 0; x < player1.mobility + 1; x++) {
+                    $("#case-" + (player1.position.a + x) + player1.position.b).toggleClass("mobility");
+                    $("#case-" + (player1.position.a - x) + player1.position.b).toggleClass("mobility");
+                    $("#case-" + player1.position.a + (player1.position.b + x)).toggleClass("mobility");
+                    $("#case-" + player1.position.a + (player1.position.b - x)).toggleClass("mobility");
+                    //Mettre des animations entrée / sortie
+                }
+            });
+            $("#case-" + player1.position.a + player1.position.b).click(function(){
+                $("#case-" + player1.position.a + player1.position.b).off();
+                for(var x = 0; x < player1.mobility + 1; x++) {
+                    $("#case-" + (player1.position.a + x) + player1.position.b).toggleClass("mobility");
+                    $("#case-" + (player1.position.a - x) + player1.position.b).toggleClass("mobility");
+                    $("#case-" + player1.position.a + (player1.position.b + x)).toggleClass("mobility");
+                    $("#case-" + player1.position.a + (player1.position.b - x)).toggleClass("mobility");
+                    //Mettre des animations entrée / sortie
+                }
+                $("#case-" + player1.position.a + player1.position.b).removeClass('robot0');
+                player1.move();
+                    for(var i = 0; i < board.walls.length; i++) {
+                        if((player1.position.a == wallsPosition[i].position.a) && (player1.position.b == wallsPosition[i].position.b)) {
+                            //$("#case-" + player1.position.a + player1.position.b).off();
+                            console.log("ne doit pas passer");
+                        }
+                    }
+                $("#case-" + player1.position.a + player1.position.b).addClass('robot0');
+                //Si le personnage arrive sur une case avec une arme, il prend l'arme et laisse la sienne sur place
+                //Si le personnage veut se déplacer à travers un mur ou un autre personnage, il s'arrête devant
+                //Si le personnage arrive à côté de l'autre personnage le combat commence
+            });
         });
-    });
-    //turn = true;
-//}
+    }
+
+    if(turn == false) {  
+        $(document).ready(function(){
+            console.log("Tour du Joueur 2");
+            $("#case-" + player2.position.a + player2.position.b).click(function(){
+                $("#case-" + player2.position.a + player2.position.b).removeClass('robot1');
+                player2.move();
+                $("#case-" + player2.position.a + player2.position.b).addClass('robot1');
+            });
+        });
+    }
+});
 
 // Conditions de victoire = lifePoints à 0
-if((joueur1.lifePoints == 0) || (joueur2.lifePoints == 0)) {
+if((player1.lifePoints == 0) || (player2.lifePoints == 0)) {
     victoryCondition = true;
     if(victoryCondition == true) {
-        if(joueur1.lifePoints == 0) {
-            alert("Le " + joueur2.name + " a gagné !");
+        if(player1.lifePoints == 0) {
+            alert("Le " + player2.name + " a gagné !");
         } else {
-            alert("Le " + joueur1.name + " a gagné !");
+            alert("Le " + player1.name + " a gagné !");
         }
     }
 }
