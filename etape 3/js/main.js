906db38cdd63866d43245423095dc3d91dc79684
Guillaume <g.erckelbout@gmail.com>
2018-06-18 17:02:17 +0200

    Réorganisation du code (game.js) ajout des informations des joueurs dans l'HTML

--- etape 3/js/main.js
+++ etape 3/js/main.js
@@ -1,10 +1,2 @@
-var board = new Grid({width: 10, height: 10});
-board.createGrid();
-board.populateGridWith("wall");
-board.populateGridWith("weapon");
-board.populateGridWith("robot");
-
-var actualPlayer = board.robots[0];
-var nextPlayer = board.robots[1];
-
-actualPlayer.move(board);
+var game = new Game();
+game.startGame();
--- etape 3/js/robot.js
+++ etape 3/js/robot.js
@@ -13,14 +13,6 @@ class Robot {
         board.setMobilityWithDirection("bottom", this);
         board.setMobilityWithDirection("left", this);
         board.setMobilityWithDirection("right", this);
-        
-        $(".mobility").click((e) => {
-            this.changePosition(e.currentTarget);
-            this.checkWeapon(e.currentTarget);
-            this.beginBattle();
-            $(".mobility").removeClass("mobility").off();
-            turn();
-        });
     }
     
     changePosition(target) {
@@ -44,56 +36,20 @@ class Robot {
         });
     }
     
-    beginBattle() {
-        $.each(Config.robotsElements, (numberObject, objectSkills) => {
-            if(
-                $("#case-" + (this.position.a + 1) + this.position.b).hasClass(objectSkills.classCSS) ||
-                $("#case-" + (this.position.a - 1) + this.position.b).hasClass(objectSkills.classCSS) ||
-                $("#case-" + this.position.a + (this.position.b + 1)).hasClass(objectSkills.classCSS) ||
-                $("#case-" + this.position.a + (this.position.b - 1)).hasClass(objectSkills.classCSS)
-            ) {
-                alert("Début du combat");
-                actualPlayer.mobility = 0;
-                nextPlayer.mobility = 0;
-            }
-        });
+    attack(target) {
+        this.attackCalculation(target);
     }
     
-    battle() {
-        $( () => {
-            $("#actionChoice").dialog({
-                buttons: {
-                    Attaque: () => {
-                        this.attack();
-                        turn();
-                    },
-                    Défense: () => {
-                        this.protect();
-                        turn();
-                    }
-                }
-            });
-        });
-        console.log(`Action de ${this.name}
-        Attaquer avec ${this.weapon.name} ${this.weapon.power} points de dégâts 
-        Défendre (Réduit les dégâts de la prochaine attaque de 50%)`);
-    }
-    
-    attack() {
-        this.attackCalculation();
-        victoryCondition();
-    }
-    
-    attackCalculation() {
+    attackCalculation(target) {
         console.log(`${this.name} attaque avec l'arme ${this.weapon.name} et inflige ${this.weapon.power} points de dégâts`);
-        if($(`.${nextPlayer.classCSS}`).hasClass("shield")) {
-            nextPlayer.lifePoints = nextPlayer.lifePoints - (this.weapon.power / 2);
-            $(`.${nextPlayer.classCSS}`).removeClass("shield");
-            console.log(`${nextPlayer.name} a absorbé ${this.weapon.power / 2}`);
+        if($(`.${target.classCSS}`).hasClass("shield")) {
+            target.lifePoints = target.lifePoints - (this.weapon.power / 2);
+            $(`.${target.classCSS}`).removeClass("shield");
+            console.log(`${target.name} a absorbé ${this.weapon.power / 2}`);
         } else {
-            nextPlayer.lifePoints = nextPlayer.lifePoints - this.weapon.power;      
+            target.lifePoints = target.lifePoints - this.weapon.power;      
         }  
-        console.log(`Il reste ${nextPlayer.lifePoints} points de vie au ${nextPlayer.name}`);
+        console.log(`Il reste ${target.lifePoints} points de vie au ${target.name}`);
     }
     
     protect() {
